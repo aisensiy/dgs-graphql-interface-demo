9638cacb78c04b138e42f80ec9c3dafeadfeb916
aisensiy <aisensiy@163.com>
2022-02-20 19:04:13 +0800

    init commit

---
+++ src/main/java/com/example/dgsgraphql/DgsGraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.dgsgraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DgsGraphqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DgsGraphqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dgsgraphql/DgsGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dgsgraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DgsGraphqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
